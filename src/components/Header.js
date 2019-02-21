3ddb0769ea6c71260a6287e286ad1f170111b8dc
priyankabehera <priyanka.behera@multidots.com>
2019-02-21 17:57:06 +0530

    news link

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,6 +35,7 @@ const Header = (props) => (
         <ul className="primary-menu">
             <li><a href="#">Home</a></li>
             <li><a href="/ahmedabad">Ahmedabad</a></li>
+            <li><a href="/news">News</a></li>
             <li><a href="/contact">Contact</a></li>
             <li><a href="/about">About</a></li>
         </ul>
